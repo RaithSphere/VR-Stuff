56f26a3c2c3597d094944cade12a85b8f9b6417c
RaithSphere Starpaw <raithsphere@gmail.com>
2020-10-29 11:05:16 +0000

    Create data.py

---
+++ data.py
@@ -0,0 +1,65 @@
+"""
+Service Explorer
+----------------
+An example showing how to access and print out the services, characteristics and
+descriptors of a connected GATT server.
+Created on 2019-03-25 by hbldh <henrik.blidh@nedomkull.com>
+"""
+import platform
+import asyncio
+import logging
+
+from bleak import BleakClient
+
+
+async def run(address, debug=False):
+    log = logging.getLogger(__name__)
+    if debug:
+        import sys
+
+        log.setLevel(logging.DEBUG)
+        h = logging.StreamHandler(sys.stdout)
+        h.setLevel(logging.DEBUG)
+        log.addHandler(h)
+
+    async with BleakClient(address) as client:
+        x = await client.is_connected()
+        log.info("Connected: {0}".format(x))
+
+        for service in client.services:
+            log.info("[Service] {0}: {1}".format(service.uuid, service.description))
+            for char in service.characteristics:
+                if "read" in char.properties:
+                    try:
+                        value = bytes(await client.read_gatt_char(char.uuid))
+                    except Exception as e:
+                        value = str(e).encode()
+                else:
+                    value = None
+                log.info(
+                    "\t[Characteristic] {0}: (Handle: {1}) ({2}) | Name: {3}, Value: {4} ".format(
+                        char.uuid,
+                        char.handle,
+                        ",".join(char.properties),
+                        char.description,
+                        value,
+                    )
+                )
+                for descriptor in char.descriptors:
+                    value = await client.read_gatt_descriptor(descriptor.handle)
+                    log.info(
+                        "\t\t[Descriptor] {0}: (Handle: {1}) | Value: {2} ".format(
+                            descriptor.uuid, descriptor.handle, bytes(value)
+                        )
+                    )
+
+
+if __name__ == "__main__":
+    address = (
+        "FD:CF:5F:4C:74:83"
+        if platform.system() != "Darwin"
+        else "B9EA5233-37EF-4DD6-87A8-2A875E821C46"
+    )
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    loop.run_until_complete(run(address, True))
