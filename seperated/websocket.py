3ceca93ac4e9dbcf8b392104c2725d2821637ffd
raithsphere <raithsphere@gmail.com>
2020-12-11 10:21:00 +0000

    Implement whitelist system to the websocket to stop files that shouldn't be accessed

--- seperated/websocket.py
+++ seperated/websocket.py
@@ -1,14 +1,40 @@
+import json
+import os
+
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+data = "";
+
+
+def loadwhitelist():
+    global data
+    confpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "whitelist.json")
+    if os.path.exists(confpath):
+        with open("whitelist.json") as json_data_file:
+            data = json.load(json_data_file)
+    else:
+        print("No Config file found")
+        exit(0)
 
 
 class SimpleEcho(WebSocket):
+
     def handleMessage(self):
-        print(self.data)
-        file = self.data
-        textfile = open("%s" % file)
-        data = textfile.read()
-        self.sendMessage(data)
-        textfile.close()
+        global data
+        loadwhitelist()
+
+        filename = self.data.rsplit('\\', 1)[-1]
+
+        if filename in data['whitelist']:
+            print(self.data)
+            file = self.data
+            textfile = open("%s" % file)
+            data = textfile.read()
+            self.sendMessage(data)
+            textfile.close()
+        else:
+            self.sendMessage("Access to this file is not allowed under whitelist policy")
+            print("Not found on whitelist %s" % self.data)
 
     def handleConnected(self):
         print(self.address, "connected")
@@ -17,5 +43,6 @@ class SimpleEcho(WebSocket):
         print(self.address, "closed")
 
 
+print("Starting Simple Websocket")
 server = SimpleWebSocketServer("", 8123, SimpleEcho)
 server.serveforever()
